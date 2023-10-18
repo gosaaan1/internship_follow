16b97d35ac7d7d6ee3e8e7d45e843901399ab4f0
tyamaguchi <t_yamaguchi@webfrontier.co.jp>
2023-10-18 17:55:25 +0900

    TailwindCSSが適用されない問題を修正
    - Would you like to use App Router? で Noにする必要があった。
    - 顔検知サーバをJSONPlaceholderに置き換えて通信を検証

--- face-detection/pages/api/upload.ts
+++ face-detection/pages/api/upload.ts
@@ -18,6 +18,11 @@ const API_KEY: string = '7700b4a7-bd1f-4063-9115-7f125371cf80';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
+    const formData = new FormData();
+    const customHeaders = {
+      'x-api-key': API_KEY
+    };
+
     new Promise((resolve, reject) => {
       // フォームデータの解析
       // https://qiita.com/takamura_s/items/b7cbfbb5ba2879b81f5c
@@ -43,25 +48,39 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       formData.append('file', file);
 
       const customHeaders = {
+        'content-Type': 'multipart/form-data',
         'x-api-key': API_KEY
       };
 
       console.log(`POST to ${API_URL}`);
-      fetch(API_URL, {
-        method: 'POST',
-        headers: customHeaders,
-        body: formData
+      // fetch(API_URL, {
+      //   method: 'POST',
+      //   headers: customHeaders,
+      //   body: formData
+      fetch('https://jsonplaceholder.typicode.com/todos/1', {
+        method: 'GET',
       }).then((response) => {
-        // TODO 顔検知サーバーからレスポンスが返ってこないよぉ(T_T)
-        console.log(`post response ${response.json()}`);
         return response.json();
       }).then((jsonData) => {
+        console.log(jsonData);
         res.status(200).json(jsonData);
       }).catch((error) => {
         console.log(error);
         res.status(200).json({'message': 'post failed'});
       });
     });
+
+    // fetch('https://jsonplaceholder.typicode.com/todos/1', {
+    //   method: 'GET',
+    // }).then((response) => {
+    //   return response.json();
+    // }).then((jsonData) => {
+    //   console.log(jsonData);
+    //   res.status(200).json(jsonData);
+    // }).catch((error) => {
+    //   console.log(error);
+    //   res.status(200).json({'message': 'post failed'});
+    // });
   } else {
     return res.status(405).end();
   }
--- face-detection/pages/index.tsx
+++ face-detection/pages/index.tsx
@@ -1,9 +1,12 @@
+import { Inter } from 'next/font/google'
 import FaceDetection from '../components/FaceDetection';
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <div className="container mx-auto">
       <FaceDetection />
     </div>
-  );
+  )
 }
